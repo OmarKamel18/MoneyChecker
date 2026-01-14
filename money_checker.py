0e4f31674827c1e62c999ed74b61f80ba83040da
OmarKamel18 <d3d3rrfe3@gmail.com>
2026-01-14 03:27:56 +0200

    Add files via upload

---
+++ money_checker.py
@@ -0,0 +1,8 @@
+m=int(input("How much money do you have? 💰 : "))
+if m>10000:
+      print("Can you hire me?😭")
+elif m>5000:
+      print(f"{m} Cool, you’re doing great!!")
+else :
+      print("do you live in egypt???")
+
